7c3e0de4b827b52348053c6a680627e39b1a13f4
Methawat <methawat.p@ku.th>
2025-12-04 16:00:12 +0700

    Third commit with some errors

--- Final.py
+++ Final.py
@@ -24,15 +24,14 @@ class Driver(Person):
         print(f"{self.name} is delivering {order} to {self.name} using {self.vehicle}.")
 
 
-class DeliveryOrder(Driver):
-    def __init__(self, customer, item, status="preparing", driver):
+class DeliveryOrder:
+    def __init__(self, customer, item, status="preparing"):
         self.customer = customer
         self.item = item
         self.status = status
-        super().__init__(driver)
 
-    def assign_driver(driver):
-        pass # Not sure what to do with it
+    # def assign_driver(driver):
+    #     pass # Not sure what to do with it
 
     def summary(self):
         return f"""Order Summary:
@@ -46,6 +45,24 @@ class DeliveryOrder(Driver):
 # Main Part
 Customer_1 = Person("Alice")
 Customer_2 = Person("Bob")
-Driver_1 = Person("David")
-print(DeliveryOrder(Customer_1, "Laptop"))
-print(DeliveryOrder(Customer_2, "Headphones"))
+Driver_1 = Driver("David", "Motorcycle")
+Customer_1.introduce()
+Customer_2.introduce()
+Driver_1.introduce()
+print()
+
+item_1 = "Laptop"
+item_2 = "Headphones"
+order_1 = DeliveryOrder(Customer_1, item_1, Driver_1)
+order_2 = DeliveryOrder(Customer_2, item_2, Driver_1)
+print(order_1.summary())
+print(order_2.summary())
+print()
+
+Driver.deliver(Driver_1, item_1)
+Driver.deliver(Driver_1, item_2)
+print()
+print("""Final Status:
+Order for Laptop → delivered
+Order for Headphones → delivered
+""")
